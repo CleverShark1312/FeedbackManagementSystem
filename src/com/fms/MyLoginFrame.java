6d9bf0f6e3629c03ed9b0d68124b20fe7b252421
Harshita Srivastava <mailmeharshita24@gmail.com>
2023-07-30 10:15:07 +0530

    Working on loginUI

--- src/com/fms/MyLoginFrame.java
+++ src/com/fms/MyLoginFrame.java
@@ -1,10 +1,30 @@
 package com.fms;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyLoginFrame extends JFrame {
-    public MyLoginFrame(){
+    JPanel loginPanel = new JPanel();
+    JTextField textField = new JTextField(20);
+    JPasswordField pswdField = new JPasswordField(20);
+    JButton loginButton = new JButton("LOGIN");
+    JLabel userName = new JLabel("Name");
+    
+    JLabel password = new JLabel("Password");
+    private void loginUi(){
+        getContentPane().add(loginPanel);
+
+        loginPanel.add(userName);
+        loginPanel.add(textField);
+        loginPanel.add(password);
+        loginPanel.add(pswdField);
+        loginPanel.add(loginButton);
+
+
+    }
+    public MyLoginFrame() {
         super("Login Page");
+        loginUi();
 
     }
 }
