5ee4cc4b148ff6e7eb6e86fe4f37987c51f96788
Harshita Srivastava <mailmeharshita24@gmail.com>
2023-07-31 10:08:48 +0530

    Login functionality implemented.

--- src/com/fms/MyLoginFrame.java
+++ src/com/fms/MyLoginFrame.java
@@ -6,39 +6,67 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyLoginFrame extends JFrame implements ActionListener {
-    JPanel loginPanel = new JPanel();
-    JTextField textField = new JTextField(20);
-    JPasswordField pswdField = new JPasswordField(20);
+    Container container = getContentPane();
+    JLabel userLabel = new JLabel("USERNAME:-");
+    JLabel passwordLabel = new JLabel("PASSWORD:-");
+    JTextField userTextField = new JTextField();
+    JPasswordField passwordField = new JPasswordField();
     JButton loginButton = new JButton("LOGIN");
-    JLabel userName = new JLabel("Name");
-    JLabel password = new JLabel("Password");
-    JLabel success = new JLabel();
-    private void loginUi(){
-      add(loginPanel);
+    JButton resetButton = new JButton("RESET");
+    JCheckBox showPassword = new JCheckBox("Show Password");
 
-        loginPanel.add(userName);
-        loginPanel.add(textField);
-        loginPanel.add(password);
-        loginPanel.add(pswdField);
-        loginPanel.add(loginButton);
-        loginPanel.add(success);
+    public void setLayoutManager() {
+        container.setLayout(null);
+    }
+
+    public void setLocationAndSize() {
+        userLabel.setBounds(50, 150, 100, 30);
+        passwordLabel.setBounds(50, 220, 100, 30);
+        userTextField.setBounds(150, 150, 150, 30);
+        passwordField.setBounds(150, 220, 150, 30);
+        showPassword.setBounds(150, 250, 150, 30);
+        loginButton.setBounds(200, 300, 100, 30);
+      //  resetButton.setBounds(200, 300, 100, 30);
+
+
+    }
+    public void addComponentsToContainer() {
+        container.add(userLabel);
+        container.add(passwordLabel);
+        container.add(userTextField);
+        container.add(passwordField);
+        container.add(showPassword);
+        container.add(loginButton);
+        container.add(resetButton);
+    }
+
+    public void addActionEvent(){
         loginButton.addActionListener(this);
     }
-    public MyLoginFrame() {
-        super("Login Page");
-        loginUi();
 
+    public  MyLoginFrame(){
+        super("LOGIN PAGE");
+        setLayoutManager();
+        setLocationAndSize();
+        addComponentsToContainer();
+        addActionEvent();
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        String name = textField.getText();
-        String password = pswdField.getText();
-        System.out.println(name+" "+ password);
-        if(name.equals("Harshita")&& password.equals("345")){
-            success.setText("Login Successful!");
-        }else{
-            success.setText("Access denied");
+        if(e.getSource() == loginButton){
+            String userText;
+            String pswdText;
+            userText = userTextField.getText();
+            pswdText = passwordField.getText();
+            if(userText.equals("Harshita") && pswdText.equals("345")){
+                JOptionPane.showMessageDialog(this,"Login Successful");
+            }else{
+                JOptionPane.showMessageDialog(this, "Invalid username or password");
+            }
         }
+
 
     }
 }
+
